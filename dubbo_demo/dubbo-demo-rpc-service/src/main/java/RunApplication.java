887a7248534d650529afbeed1e4d980e05f0bb4e
Xiang <xiangguangye@sunline.cn>
2020-07-20 16:42:06 +0800

    add dubbo-springboot module

---
+++ dubbo_demo/dubbo-demo-rpc-service/src/main/java/RunApplication.java
@@ -0,0 +1,10 @@
+/**
+ * @Author：xiangguangye
+ * @Desc：
+ * @Date：2020/7/16 10:20
+ */
+public class RunApplication {
+    public static void main(String[] args) {
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
---
+++ dubbo_springboot/dubbo-boot-api/src/main/java/com/study/boot/service/TestService.java
@@ -0,0 +1,14 @@
+package com.study.boot.service;
+
+import com.study.frame.base.BaseService;
+
+/**
+ * @Author：xiangguangye
+ * @Desc：
+ * @Date：2020/7/20 11:10
+ */
+public interface TestService {
+
+    String sayHello(String name);
+
+}
