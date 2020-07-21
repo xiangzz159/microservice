ca0940166a82ef7f275ce675708f29fb9ef0d0a3
xiangguangye <xiangguangye@sunline.com>
2020-07-21 20:41:13 +0800

    update code

---
+++ dubbo_springboot/dubbo-boot-service/src/main/java/ApplicationRun.java
@@ -0,0 +1,6 @@
+public class ApplicationRun {
+
+    public static void main(String[] args) {
+        com.alibaba.dubbo.container.Main.main(args);
+    }
+}
--- dubbo_springboot/dubbo-boot-service/src/main/java/com/study/api/rpc/service/TestServiceImpl.java
+++ dubbo_springboot/dubbo-boot-service/src/main/java/com/study/api/rpc/service/TestServiceImpl.java
@@ -1,10 +1,20 @@
 package com.study.api.rpc.service;
 
+import com.study.boot.servicemock.TestServiceMock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * @Author：xiangguangye
  * @Desc：
  * @Date：2020/7/21 16:11
  */
-public class TestServiceImpl {
+public class TestServiceImpl extends TestServiceMock {
+
+    Logger logger = LoggerFactory.getLogger(TestServiceImpl.class);
+
+    @Override
+    public String sayHello(String name) {
+        return "Hello, " + name;
+    }
 }
